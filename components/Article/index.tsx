8f28ed74d7e83cb25f996d76b654e61f5ed1e033
juraelba <trepechovdimitar@gmail.com>
2022-06-06 18:26:16 +0300

    news articles mobile responsive

--- components/Article/index.tsx
+++ components/Article/index.tsx
@@ -2,11 +2,18 @@ import React, { useState } from "react";
 import Image from "next/image";
 import Router from "next/router";
 import DeleteModal from "../DeleteModal";
+import Button from "../common/Button";
 import { Article as ArticleProps } from "../../types";
 import {
   ActionContainer,
+  Author,
+  AuthorLabel,
   Body,
+  ButtonContainer,
+  Calendar,
+  Date,
   DeleteButton,
+  DetailInfo,
   EditButton,
   FormItem,
   Header,
@@ -15,8 +22,10 @@ import {
   Title,
   Value,
 } from "./styles";
+import useIsMobile from "../../hooks/useIsMobile";
 
 const Article: React.FC<ArticleProps> = (props) => {
+  const isMobile = useIsMobile();
   const [isOpen, setIsOpen] = useState(false);
   const handleRedirect = () => {
     Router.push(`/articles/${props.id}/view`);
@@ -35,15 +44,80 @@ const Article: React.FC<ArticleProps> = (props) => {
     <ArticleContainer onClick={handleRedirect}>
       <Header>
         <Title>{props.title}</Title>
+        {isMobile && (
+          <DetailInfo>
+            <Date>
+              <Calendar>
+                <Image
+                  src="/assets/images/icons/calendar.svg"
+                  width={24}
+                  height={24}
+                  alt=":( Not Found"
+                />
+              </Calendar>
+              {props.date}
+            </Date>
+            <Author>
+              <AuthorLabel>Created by</AuthorLabel>
+              {props.author}
+            </Author>
+          </DetailInfo>
+        )}
         <ActionContainer>
-          <EditButton
-            onClick={(e: React.MouseEvent<HTMLButtonElement>) => handleEdit(e)}
-          >
-            <Image src="/assets/images/icons/edit.svg" width={24} height={24} alt=":( Not Found" />
-          </EditButton>
-          <DeleteButton onClick={(e: React.MouseEvent<HTMLButtonElement>) => handleOpen(e)}>
-            <Image src="/assets/images/icons/trash.svg" width={24} height={24} alt=":( Not Found" />
-          </DeleteButton>
+          {isMobile ? (
+            <>
+              <ButtonContainer>
+                <Button
+                  size="sm"
+                  onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
+                    handleEdit(e)
+                  }
+                  fullWidth
+                >
+                  Edit card
+                </Button>
+              </ButtonContainer>
+              <ButtonContainer>
+                <Button
+                  size="sm"
+                  onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
+                    handleOpen(e)
+                  }
+                  fullWidth
+                  color="light-danger"
+                >
+                  Remove card
+                </Button>
+              </ButtonContainer>
+            </>
+          ) : (
+            <>
+              <EditButton
+                onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
+                  handleEdit(e)
+                }
+              >
+                <Image
+                  src="/assets/images/icons/edit.svg"
+                  width={24}
+                  height={24}
+                  alt=":( Not Found"
+                />
+              </EditButton>
+              <DeleteButton
+                onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
+                  handleOpen(e)
+                }
+              >
+                <Image
+                  src="/assets/images/icons/trash.svg"
+                  width={24}
+                  height={24}
+                  alt=":( Not Found"
+                />
+              </DeleteButton>
+            </>
+          )}
         </ActionContainer>
       </Header>
       <Body>
